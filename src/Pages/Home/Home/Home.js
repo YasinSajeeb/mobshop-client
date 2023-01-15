b3b7f388ed5a49125011a4e432950dc9baea147d
YasinSajeeb <yeasinalhossen@gmail.com>
2023-01-15 20:38:13 +0600

    ccounter

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
+import Counter from '../Counter/Counter';
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
+            <Counter></Counter>
         </div>
     );
 };
